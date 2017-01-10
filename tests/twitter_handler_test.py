856374e45a37d9703c533eeacd4b86e7d41c1596
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2017-01-10 00:25:41 -0200

    Adds twitter_handler new functions' tests

--- tests/twitter_handler_test.py
+++ tests/twitter_handler_test.py
@@ -1,14 +1,12 @@
 import sys
 import os
 import unittest
-
+from datetime import datetime, timedelta
 sys.path.insert(0, os.path.realpath('./') + '/..')
+from twitter_handler import TwitterHandler, parse_link_from_text, humanize_date, datetime_from_string
 
-
-from twitter_handler import TwitterHandler, parse_link_from_text
 
 class APITwitterHanderTest(unittest.TestCase):
-
     def setUp(self):
         self.app = TwitterHandler()
 
@@ -41,6 +39,26 @@ class APITwitterHanderTest(unittest.TestCase):
         link_expected = "Not known"
         result = parse_link_from_text(text)
         self.assertEqual(result, link_expected)
+
+    def test_datetime_from_string(self):
+        text = "Sat Jan 7 18:11:26 +0000 2017"
+        expected = datetime(2017, 1, 7, 18, 11, 26)
+        result = datetime_from_string(text)
+        self.assertEqual(result, expected)
+
+    def test_datetime_from_empty_string(self):
+        result = datetime_from_string('')
+        self.assertEqual(result, None)
+
+    def test_humanize_date_should_return_just_now(self):
+        date = datetime.now()
+        expected = 'just now'
+        result = humanize_date(date)
+        self.assertEqual(result, expected)
+
+    def test_humanize_empty_date(self):
+        result = humanize_date('')
+        self.assertEqual(result, 'Not known')
 
 
 if __name__ == '__main__':
